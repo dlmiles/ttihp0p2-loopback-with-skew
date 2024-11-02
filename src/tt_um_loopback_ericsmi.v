27ac1ba285b6d0e77aee7d5b8ddddb49f6b4cf57
Darryl L. Miles <darryl.miles@darrylmiles.org>
2024-11-02 20:42:39 +0000

    Import files from https://github.com/ericsmi/tt05-loopback-with-skew

---
+++ src/tt_um_loopback_ericsmi.v
@@ -0,0 +1,173 @@
+/*
+ * tt_um_loopback_ericsmi.v
+ *
+ * Loopback test user module, with basic skew measurement,
+ * and clock jitter measurement flop
+ *
+ * Extended from:
+ * https://github.com/TinyTapeout/tt05-loopback
+ *
+ * Author: Eric Smith
+ */
+
+`default_nettype none
+`timescale 1ns/10ps
+
+/////////////////////////////////////////////////////////////
+`define BEHAV
+`define LB_DELAY
+/////////////////////////////////////////////////////////////
+// helper modules
+
+module buff2(input I, output Z);
+`ifdef BEHAV
+  assign `LB_DELAY Z = I;
+`else
+  sky130_fd_sc_hd__buf_2 sky130_fd_sc_hd__buf_2(.A(I),.X(Z));
+`endif
+endmodule
+
+module split(input I, output A,B,C);
+  wire t;
+  buff2 buff2(.I(I),.Z(t));
+  buff2 buff2a(.I(t),.Z(A));
+  buff2 buff2b(.I(t),.Z(B));
+  buff2 buff2c(.I(t),.Z(C));
+endmodule
+
+module rdffe(input D,E,CLK,RSTN, output Q);
+  reg FF;
+  always @(negedge RSTN or posedge CLK)
+    if( ~RSTN )
+	  FF <= 0;
+	else begin
+	  if (E) FF <= D;
+	end
+  assign `LB_DELAY Q = FF;
+endmodule
+
+module dff(input D,CLK, output Q);
+  reg FF;
+  always @(posedge CLK)
+    FF <= D;
+  assign `LB_DELAY Q = FF;
+endmodule
+
+module mux8(input [7:0] I, input [2:0] S, output Z);
+`ifdef BEHAV
+  assign `LB_DELAY Z = S[2] ? 
+      ( S[1] ? ( S[0] ? I[7] : I[6] ) : ( S[0] ? I[5] : I[4] ) ): 
+      ( S[1] ? ( S[0] ? I[3] : I[2] ) : ( S[0] ? I[1] : I[0] ) );
+`else
+  wire [1:0] w;
+        sky130_fd_sc_hd__mux4_2 sky130_fd_sc_hd__mux4_2_0(
+                .S1(S[1]),.S0(S[0]),
+                .A0(I[0]),.A1(I[1]),.A2(I[2]),.A3(I[3]),
+                .X(w[0]));
+        sky130_fd_sc_hd__mux4_2 sky130_fd_sc_hd__mux4_2_1(
+                .S1(S[1]),.S0(S[0]),
+                .A0(I[4]),.A1(I[5]),.A2(I[6]),.A3(I[7]),
+                .X(w[1]));
+        sky130_fd_sc_hd__mux2_2 sky130_fd_sc_hd__mux2_2(
+                .S(S[2]),
+                .A0(w[0]),.A1(w[1]),
+                .X(Z));
+`endif
+endmodule
+
+/////////////////////////////////////////////////////////////
+
+module tt_um_dlmiles_loopback (
+	input  wire [7:0] ui_in,	// Dedicated inputs
+	output wire [7:0] uo_out,	// Dedicated outputs
+	input  wire [7:0] uio_in,	// IOs: Input path
+	output wire [7:0] uio_out,	// IOs: Output path
+	output wire [7:0] uio_oe,	// IOs: Enable path (active high: 0=input, 1=output)
+	input  wire       ena,
+	input  wire       clk,
+	input  wire       rst_n
+);
+
+    wire [7:0] ui_ina; // for measurement
+    wire [7:0] ui_inb; // for internal use, avoid loading measurement
+    wire [7:0] ui_inc; // for measurement
+
+    wire rst_n_a, rst_n_b, rst_n_c;
+
+    wire en,D,mCLK,Qp,Qp_bypassed,Qn,Qn_bypassed,Qj;
+
+    wire default_mode_n; // when 0, match the behavior of tt05-loopback
+    wire bypass;
+
+    reg [3:0] clk_div;
+
+    split split_rst_n (
+        .I(rst_n),
+        .A(rst_n_a), 
+        .B(rst_n_b),
+        .C(rst_n_c) 
+    );
+
+    split split_ui_in [7:0] (
+        .I(ui_in[7:0]),
+        .A(ui_ina[7:0]), 
+        .B(ui_inb[7:0]),
+        .C(ui_inc[7:0]) 
+    );
+
+    rdffe rdffe_uio [7:0] (
+        .D(uio_in[7:0]),
+        .E({8{en}}),
+        .CLK(clk),
+        .RSTN({8{rst_n_a}}),
+        .Q(uio_out[7:0])
+    );
+
+    rdffe rdffe_jitter (
+        .D(~Qj),
+      	.E(en & default_mode_n),
+        .CLK(clk),
+	      .RSTN(rst_n_b),
+	      .Q(Qj)
+    );
+
+    assign bypass = uio_out[7];
+    assign default_mode_n = uio_out[6];
+
+    assign en = &ui_inb[7:4];
+    assign uio_oe[7:0] = {8{~en}};
+
+    assign uo_out[3:0] = default_mode_n ? {4{Qp_bypassed}} : {4{ui_inb[0]}};
+    assign uo_out[4] = default_mode_n ? Qn_bypassed : ui_inb[0];
+    assign uo_out[5] = default_mode_n ? Qj : ui_inb[0]; 
+    assign uo_out[6] = ui_inb[0];  // same as tt05-loopback
+
+    // connectivity for all inputs
+    // technically ena should always be high, so force en=0 then toggle rst_n
+    // this should make an ok RTT for rst_n possible
+
+    assign uo_out[7] = rst_n_c ? en : ena; 
+
+    // This structure does its best to keep in-tile skew the same.
+    // I can't account for PnR on TT but the buffers should dominate the delay
+    // and we ensure there are the same numbers and sizes of those.
+    // As this tile is mostly empty I anticapte the flow should do ok at PnR. 
+
+    mux8 mux8_d( .I(ui_ina[7:0]), .S(uio_out[2:0]), .Z(D));   
+    mux8 mux8_c( .I(ui_inc[7:0]), .S(uio_out[5:3]), .Z(mCLK)); 
+    
+    // This is the phase measurement
+    //   Start with Q=0, 
+    //   Then put a rising edge on D and CLK on the outside of the IC
+    //   If Q=0 then mCLK arrived before D, if Q=1 then D arrived before mCLK
+    //   This allows racing inputs vs one another. 
+    //   The amount of delay adjust needed on outside of the IC to make 
+    //   Q toggle is the amount of skew in path from IC input pin to tile input pin. 
+
+    dff dffp( .D(D), .CLK(mCLK), .Q(Qp));  // test arrival of posedge
+    dff dffn( .D(D), .CLK(~mCLK), .Q(Qn)); // test arrival of negedge
+
+    assign Qp_bypassed = bypass ? D : Qp;
+    assign Qn_bypassed = bypass ? D : Qn;
+
+endmodule // tt_um_loopback
--- test/test.py
+++ test/test.py
@@ -1,40 +1,119 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
+
+segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
+async def test_loopback_ericsmi(dut):
+    dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
+    # Initial condition from MUX, tile not selected
     dut.ui_in.value = 0
     dut.uio_in.value = 0
+    dut._log.info("reset")
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    dut.ena.value = 0
+
+     # Clear X to make cocotb happy 
+
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 0
 
-    dut._log.info("Test project behavior")
+    await ClockCycles(dut.clk, 1)
+    assert 0x80 & dut.uo_out.value == 0x00
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Select this tile
 
-    # Wait for one clock cycle to see the output values
+    dut.ena.value = 1  
+    await ClockCycles(dut.clk, 1)
+    assert 0x80 & dut.uo_out.value == 0x80
+
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+    assert 0x80 & (dut.uo_out.value) == 0x00
+
+    dut.ui_in.value = 0xF0
+    await ClockCycles(dut.clk, 1)
+    assert 0x80 & (dut.uo_out.value) == 0x80
+
+    dut.ui_in.value = 0xE0
+    await ClockCycles(dut.clk, 1)
+    assert 0x80 & (dut.uo_out.value) == 0x00
+
+    dut._log.info("PASS: buffer ena and &ui_in[7:4]")
+
+    for k in range(2):
+        # default behavior ignores rst_n
+        dut.rst_n.value = k
+        for i in range(4):
+            dut.ui_in.value = i
+            await ClockCycles(dut.clk, 1)
+            v = int(dut.uo_out.value)
+            for j in range(7):
+                assert 0x1 & v == 0x1 & i
+                v >>= 1
+
+    dut._log.info("PASS: buffer i[0]")
+
+    for i in range(8):
+        for j in range(8):
+            if i != j:
+                dut.ui_in.value = 0xF0
+                dut.uio_in.value = 0x40 | (j<<3) | i
+                await ClockCycles(dut.clk, 1)
+                dut.ui_in.value = 0
+                await ClockCycles(dut.clk, 1)
+                assert dut.uio_in.value == dut.uio_out.value
+                for k in range(2):
+                    dut.ui_in.value = k << i # assign D
+                    await ClockCycles(dut.clk, 1) # make posedge
+                    dut.ui_in.value = (k << i)|(1<<j)
+                    await ClockCycles(dut.clk, 1)
+                    dut.ui_in.value = k << i # make negedge
+                    await ClockCycles(dut.clk, 1)
+                    assert k == 0x1 & int(dut.uo_out.value)
+                    assert k == 0x1 & (int(dut.uo_out.value) >>4)
+
+    dut._log.info("PASS: Race Bits")
+
+    dut.ui_in.value = 0xF0
+    # enable bypass and use sel0=1
+    dut.uio_in.value = 0xC1
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 1)
+    assert 0xC1 & int(dut.uio_out.value)
+
+    for i in range(4):
+        dut.ui_in.value = i
+        await ClockCycles(dut.clk, 1)
+        assert i & 0x1 == int(dut.uo_out.value) >> 6
+        assert i >> 1 == 0x1 & int(dut.uo_out.value)
+
+    dut._log.info("PASS: Bypass Mode")
+    
+    dut.ui_in.value = 0xF0 # turn on jitter flop
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    prev = int(dut.uo_out.value) >> 5
+    await ClockCycles(dut.clk, 1)
+    next = int(dut.uo_out.value) >> 5
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    assert prev != next 
+
+    dut.ui_in.value = 0xE0 # turn off jitter flop
+    await ClockCycles(dut.clk, 1)
+
+    prev = int(dut.uo_out.value) >> 5
+    await ClockCycles(dut.clk, 1)
+    next = int(dut.uo_out.value) >> 5
+
+    assert prev == next 
+
+    dut._log.info("PASS: clk div2")
+
